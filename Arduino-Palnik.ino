d02c802d69814970f3062c65367dfc19a024f410
Zdeněk Břicháček <zdenek@brichacek.net>
2016-03-02 17:55:11 +0100

    Měření napětí na pinech A10 a A11

--- Arduino-Palnik.ino
+++ Arduino-Palnik.ino
@@ -19,23 +19,23 @@
 #include <LiquidCrystal_I2C.h>
 #include <Streaming.h> 
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+//LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-int outputs [58] = {A12,     2, 3, 4, 5, 6, 7, 8, 9,
+int outputs [56] = {A12,     2, 3, 4, 5, 6, 7, 8, 9,
                     10, 11, 12,     14, 15, 16, 17, 18, 19,
                             22, 23, 24, 25, 26, 27, 28, 29, // I2C: 20 = SDA, 21 = SCL
                     30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
                     40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
                     A0, A1, A2, A3, A4, A5, A6, A7, A8, A9,
-                    A10, A11
                     };
-
-//int outputs_last [58] = {};
 
 #define PIN_MAN_PAL A15 // Tlačítko inkrementace výstupu
 #define PIN_MAN_MODE A14 // Přepínač módu PAL/TEST
 #define PIN_REL_CHK A13 // Výstup pro ovládání relé PAL/TEST
 #define PIN_HB 13 // Výstup hearbeat
+#define PIN_VOLT_U1 A10 // První voltmetr
+#define PIN_VOLT_U2 A11 // Druhý voltmetr
 
 int BTN_PAL_STAT_old = 0;
 int BTN_MODE_old = 1;
@@ -50,7 +50,8 @@ void setup()
   Serial.begin(9600);
   Serial.println("Starting program...");
   Serial.println(""); 
-  
+
+  lcd.init();
   lcd.begin(16,2);
   lcd.backlight();
 
@@ -61,21 +62,31 @@ void setup()
   digitalWrite(PIN_MAN_PAL, HIGH);
   pinMode(PIN_MAN_MODE, INPUT);     // Nastavení tlačítka módu
   digitalWrite(PIN_MAN_MODE, HIGH);
-  pinMode(PIN_REL_CHK, OUTPUT);     // Nastavení výstupu pro přepnutí relé pro režim odpalování
-  digitalWrite(PIN_REL_CHK, HIGH);
   pinMode(PIN_HB, OUTPUT);     // Pin 13 - heartbeat
+  pinMode(PIN_VOLT_U1, INPUT);
+  //digitalWrite(PIN_VOLT_U1, LOW);
+  pinMode(PIN_VOLT_U2, INPUT);
+  //digitalWrite(PIN_VOLT_U2, LOW);
 
-  for (out = 0 ; out < 58 ; ++out) {
+  for (out = 0 ; out < 56 ; ++out) {
     pinMode(outputs[out], OUTPUT);
     digitalWrite(outputs[out], LOW);
     Serial << "Setup PIN " << outputs[out] << " as OUTPUT/LOW." << endl;
   }
+
+  pinMode(PIN_REL_CHK, OUTPUT);     // Nastavení výstupu pro přepnutí relé pro režim odpalování
+  digitalWrite(PIN_REL_CHK, HIGH);
+
   digitalWrite(outputs[0], HIGH); // Nastavit pin 0 jako HIGH - indikace připravenosti (LED)
   lcd.print("..Ok");
   out = 0;
-  delay(2000);
+  delay(1000);
   lcd.home ();
-  lcd.print("Ready to operate.");
+//  lcd.print("Ready to operate.");
+  float voltage1 = analogRead(PIN_VOLT_U1) * 5 / 1023;
+  float voltage2 = analogRead(PIN_VOLT_U2) * 5 / 1023;
+  lcd << "U1:" << voltage1 << "V,2:" << voltage2 << "V";
+  
   BTN_MODE_old = digitalRead(PIN_MAN_MODE);
   if (BTN_MODE_old == 0) { // Inicializace režimu pro odpal
     digitalWrite(PIN_REL_CHK, HIGH); // Vypneme testovací režim - relé do HIGH
@@ -88,7 +99,7 @@ void setup()
     Serial.println("... Checking...");
     digitalWrite(PIN_REL_CHK, LOW); // Zapneme testovací režim - relé do LOW
     delay(200); // Počkáme 200 ms na přepnutí relé a budeme testovat výstup palníku
-    for (out = 0 ; out < 58 ; ++out) {
+    for (out = 0 ; out < 56 ; ++out) {
       pinMode(outputs[out], INPUT); // Nastavíme pin jako vstup
       digitalWrite(outputs[out], HIGH); // Připojíme pull-up
       if (digitalRead(outputs[out]) == LOW)
@@ -139,7 +150,7 @@ void loop()
         digitalWrite(PIN_REL_CHK, HIGH); // Ukončíme test - relé do HIGH
       }
       ++out; // Inkrementace pozice výstupu - číslo výstupu je z pole outputs[out]
-      if (out >= 58) { // Pokud jsme na konci pole, začneme znovu - je to samozřejmě zbytečné, ale hodí se pro ladění zařízení
+      if (out >= 56) { // Pokud jsme na konci pole, začneme znovu - je to samozřejmě zbytečné, ale hodí se pro ladění zařízení
         out = 0;
       }
       if (digitalRead(PIN_MAN_MODE) == 0) { // Odpálení - tlačítko MODE sepnuto
